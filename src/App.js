0cfb82ce0b01cc725471c294e96d33035dfc75dd
jprajzendanc <j.prajzendanc@gmail.com>
2022-11-21 11:57:27 +0100

    passing value to component and display

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <h1 className="App-title">React App</h1>
       </header>
-        <Counter/>
+        <Counter initValue={108}/>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -2,22 +2,20 @@ import React, {Component} from "react";
 import './Counter.css';
 
 /*function Counter(props) {
-    let randomNumber = Math.floor(Math.random() * 108);
     return (<div className="counter">
         Counter:
         <span className="value">
-            {randomNumber}
+            {props.initvalue}
         </span>
     </div>);
 }*/
 
 class Counter extends Component {
     render() {
-        let randomNumber = Math.floor(Math.random() * 108);
         return (<div className="counter">
             Counter:
             <span className="value">
-            {randomNumber}
+            {this.props.initValue}
         </span>
         </div>);
     }
