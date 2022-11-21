60a8ca44ea23ffa7fd6c6ed4376f7d753a14c01f
jprajzendanc <j.prajzendanc@gmail.com>
2022-11-21 13:45:49 +0100

    component's state

--- src/Counter.js
+++ src/Counter.js
@@ -11,12 +11,41 @@ import './Counter.css';
 }*/
 
 class Counter extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            counterValue: this.props.initValue
+        }
+        /*
+        * ES5 METHOD
+        * this.add = this.add.bind(this);
+        * */
+    }
+
+    /*
+    * ES5 METHOD
+    *
+    * add() {}
+    * */
+
+    //ES6
+    add = () => {
+        this.setState((prevState) => {
+            return ({
+               counterValue: prevState.counterValue + 1
+            });
+        })
+    }
+
     render() {
-        return (<div className="counter">
+        return (
+        <div className="counter">
             Counter:
             <span className="value">
-            {this.props.initValue}
-        </span>
+                {this.state.counterValue}
+            </span>
+            <button onClick={this.add}>Add 1</button>
         </div>);
     }
 }
